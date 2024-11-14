319994526afee974b63010eb270b046cef585ed6
techdhara047 <techdhara047@gmail.com>
2024-11-14 06:48:17 +0530

    download button

--- src/pages/Template.jsx
+++ src/pages/Template.jsx
@@ -1,13 +1,17 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { useLocation } from 'react-router-dom';
+import { toPng } from 'html-to-image';
+import { saveAs } from 'file-saver';
 import './Template.css';
 
 const Template = () => {
   const location = useLocation();
   const { file, text } = location.state || {};
+  const divRef = useRef(null);
  
   const [height, setHeight] = useState(null);
   const [width, setWidth] = useState(null);
+  const [fileName, setFileName] = useState('1');
 
   const handleHeight = (height) => {
     console.log(height);
@@ -22,40 +26,59 @@ const Template = () => {
   }
   
 
+  const handleDownload = () => {
+    if (divRef.current) {
+      toPng(divRef.current)
+        .then((dataUrl) => {
+          saveAs(dataUrl, fileName+'.png'); // Save the image
+        })
+        .catch((error) => {
+          console.error('Error generating image:', error);
+        });
+    }
+  };
+  
+
   return (
     <div className="box">
     
       <div className="inputs">   
         <h1>Template Page</h1>
         <div className="form-input">
-            <label htmlFor="">Height: </label>
-            <input type="text" placeholder='Height' onChange={(e)=> handleHeight(e.target.value)}/>
-        </div>
-        <div className="form-input">
                 <label htmlFor="">Width: </label>
                 <input type="text" placeholder='Width' onChange={(e)=> handleWidth(e.target.value)}/>
         </div>
+        <div className="form-input">
+            <label htmlFor="">Height: </label>
+            <input type="text" placeholder='Height' onChange={(e)=> handleHeight(e.target.value)}/>
+        </div>
+        
       
      
       </div>
       
-      {file && (
+     <div className="center">
+     {file && (
         
-         <div className="template"  >
-         <img
-            src={file}
-            alt="Uploaded"
-            style={{
-              height,
-              width
-          }}
-           
-           
-          />
-           <p>{text}</p>
-        </div>
-       
-      )}
+        <div className="template"  ref={divRef}>
+        <img
+           src={file}
+           alt="Uploaded"
+           style={{
+             height,
+             width
+         }}
+         />
+          <p>{text}</p>
+       </div>
+      
+     )}
+    <div className="download">
+      <input type="text" placeholder='Enter file name' onChange={(e) => setFileName(e.target.value)}/>
+    <button className='button' onClick={handleDownload}>Donwload</button>
+    </div>
+     </div>
+    
     </div>
   );
 };
